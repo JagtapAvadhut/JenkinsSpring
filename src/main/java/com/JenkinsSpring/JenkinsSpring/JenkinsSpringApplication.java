8a745ab161fe9239ea199f636e859db2cb4ceef9
JagtapAvadhut <avadhut.pvt@gmail.com>
2024-02-11 12:07:18 +0530

    just change

---
+++ src/main/java/com/JenkinsSpring/JenkinsSpring/JenkinsSpringApplication.java
@@ -0,0 +1,15 @@
+package com.JenkinsSpring.JenkinsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Just Cheak SprinG Boot Application");
+		System.out.println("......");
+		SpringApplication.run(JenkinsSpringApplication.class, args);
+	}
+
+}
