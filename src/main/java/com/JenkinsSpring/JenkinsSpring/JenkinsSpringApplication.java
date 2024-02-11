dc9d115107f52c0121471126c52df059d5678655
JagtapAvadhut <avadhut.pvt@gmail.com>
2024-02-11 12:16:06 +0530

    added Logger

--- src/main/java/com/JenkinsSpring/JenkinsSpring/JenkinsSpringApplication.java
+++ src/main/java/com/JenkinsSpring/JenkinsSpring/JenkinsSpringApplication.java
@@ -1,15 +1,25 @@
 package com.JenkinsSpring.JenkinsSpring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsSpringApplication {
+public class JenkinsSpringApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		System.out.println("Just Cheak SprinG Boot Application");
-		System.out.println("......");
-		SpringApplication.run(JenkinsSpringApplication.class, args);
-	}
 
+    public static final Logger logger = LoggerFactory.getLogger(JenkinsSpringApplication.class);
+
+    public static void main(String[] args) {
+        System.out.println("Just Cheak SprinG Boot Application");
+        System.out.println("......");
+        SpringApplication.run(JenkinsSpringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("JenkinsSpringApplication inside Main Class Logger Running");
+    }
 }
--- src/test/java/com/JenkinsSpring/JenkinsSpring/JenkinsSpringApplicationTests.java
+++ src/test/java/com/JenkinsSpring/JenkinsSpring/JenkinsSpringApplicationTests.java
@@ -1,13 +1,19 @@
 package com.JenkinsSpring.JenkinsSpring;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsSpringApplicationTests {
 
+
+	public static  final Logger logger= LoggerFactory.getLogger(JenkinsSpringApplication.class);
 	@Test
 	void contextLoads() {
+		logger.info("Its is Test Class");
+
 	}
 
 }
